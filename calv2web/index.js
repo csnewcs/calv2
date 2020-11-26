913b315f488b287c6eb4571ff8a5fa1ab8e1c742
csnewcs <cs.newcs0@gmail.com>
2020-11-26 13:12:40 +0900

    basic server

---
+++ calv2web/index.js
@@ -0,0 +1,19 @@
+const port = 8080
+
+const fs = require('fs')
+const url = require('url')
+const express = require('express')
+const app = express()
+const path = require('path').resolve()
+
+// app.set('view engine', 'ejs')
+// app.engine('html', require('ejs').renderFile)
+
+var server = app.listen(port, function () {
+    console.log('Server started')
+})
+
+app.get('/', function(req, res) {
+    res.sendFile(path + '/pages/index.html')
+    console.log('서버 접속')
+})
